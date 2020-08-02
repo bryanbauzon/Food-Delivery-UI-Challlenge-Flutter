a3241e44c66cbf23e6f1d655132e0c1832d9e10b
bryanbauzon <mrbryanbauzon@gmail.com>
2020-08-02 16:08:14 +0800

    UI FIX

--- lib/screen/checkout.dart
+++ lib/screen/checkout.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_ui_challenge/common/app-commons.dart';
-import 'package:food_delivery_ui_challenge/main.dart';
 
 class Checkout extends StatefulWidget{
   @override
@@ -10,18 +9,22 @@ class _CheckoutState extends State<Checkout>{
   String status = "Looking for your driver..";
   bool isPrepare = false;
   bool gotOrder = false;
+  IconData icData;
   void initState(){
     super.initState();
+     icData = Icons.search;
       Future.delayed(Duration(seconds:3),(){
           setState(() {
             status = "Preparing for your food...";
             isPrepare = true;
+            icData = Icons.timer;
           });
           if(isPrepare){
             Future.delayed(Duration(seconds: 5),(){
               setState(() {
                 status = "Your order is on the way...";
                 gotOrder = true;
+                icData = Icons.motorcycle;
               });
               if(gotOrder){
                 Future.delayed(Duration(seconds: 3),(){
@@ -42,15 +45,27 @@ class _CheckoutState extends State<Checkout>{
         decoration: BoxDecoration(
          color:AppCommons.appColor
         ),
-         child: Center(
-           child: Text(status,
+         child:Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+            Container(
+              height: 80,
+              width: 80,
+              decoration: BoxDecoration(
+                borderRadius:BorderRadius.circular(50),
+                color:AppCommons.white
+              ),
+              child:  Icon(icData, color:AppCommons.appColor),
+            ),
+             Text(status,
             style:TextStyle(
               color: AppCommons.white,
               fontWeight: FontWeight.bold,
               fontSize: 22
             ) 
           ),
-         ),
+           ],
+         )
       ),
     );
   }
