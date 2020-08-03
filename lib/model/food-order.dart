828664493b7f19c32214e1895565ca3bdd75eb8f
bryanbauzon <mrbryanbauzon@gmail.com>
2020-08-03 08:37:06 +0800

    Modified Food Order model

--- lib/model/food-order.dart
+++ lib/model/food-order.dart
@@ -2,14 +2,31 @@ import 'package:flutter/material.dart';
 
 class FoodOrder{
   int id;
+  int userId;
   String image;
   String name;
   double price;
 
   FoodOrder({
     @required this.id,
+    @required this.userId,
     @required this.image,
     @required this.name,
     @required this.price
   });
+   Map<String, dynamic>toMap(){
+     var map = <String, dynamic>{
+      'id':id,
+      'userId':userId,
+      'name':name,
+      'price':price,
+     };
+     return map;
+  }
+  FoodOrder.fromMap(Map<String, dynamic> map){
+      id = map['id'];
+      userId = map['userId'];
+     name = map['name'];
+     price = map['price'];
+  }
 }
