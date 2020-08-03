8e1fb835c12c9561333ff651793cee1a6979bb32
bryanbauzon <mrbryanbauzon@gmail.com>
2020-08-03 08:33:20 +0800

    Added User Model

---
+++ lib/model/user.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class User{
+  int id;
+  String username;
+
+  User({
+    Key key,
+    @required this.id,
+    @required this.username
+  });
+
+   Map<String, dynamic>toMap(){
+     var map = <String, dynamic>{
+       'id':id,
+      'username':username,
+     };
+     return map;
+  }
+  User.fromMap(Map<String, dynamic> map){
+      id = map['id'];
+      username = map['username'];
+  }
+}
