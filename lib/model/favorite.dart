8302893b0ebf823c98853fb1d860df81de3899b5
bryanbauzon <mrbryanbauzon@gmail.com>
2020-08-03 08:36:11 +0800

    Modified favorite model

--- lib/model/favorite.dart
+++ lib/model/favorite.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 class Favorite{
   int id;
+  int userId;
   String name;
   double price;
 
-  Favorite({@required this.id,@required this.name,@required this.price});
+  Favorite({Key key,
+    @required this.id,@required this.userId,@required this.name,@required this.price});
+
+   Map<String, dynamic>toMap(){
+     var map = <String, dynamic>{
+      'id':id,
+      'userId':userId,
+      'name':name,
+      'price':price,
+     };
+     return map;
+  }
+  Favorite.fromMap(Map<String, dynamic> map){
+      id = map['id'];
+      userId = map['userId'];
+     name = map['name'];
+     price = map['price'];
+  }
 }
